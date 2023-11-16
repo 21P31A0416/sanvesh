06c83c6fea91a61c82b3dccaf3436a20ee07cb08
DELL <sanveshdangeti258@gmail.com>
2023-11-16 11:42:36 +0530

    updated

--- lib/RussiaScreen.dart
+++ lib/RussiaScreen.dart
@@ -7,12 +7,79 @@ class RussiaScreen extends StatefulWidget {
 }
 
 class _RussiaScreenState extends State<RussiaScreen> {
+  int i = 0;
+  var l = [
+    FF(image: "https://cdn.pixabay.com/photo/2017/09/01/13/56/university-2704306_1280.jpg", Text: "Colleges", text: "/RussiaColleges"),
+    FF(image: "https://media.istockphoto.com/id/1278630367/photo/grow-green-trees-on-money-in-energy-saving-light-bulbs-including-graduation-hats-with-ideas.webp?b=1&s=612x612&w=0&k=20&c=UTp8mUy55MyDTnvU31XndWI2l2Yjppz1QlY82EvOEjY=", Text: "Scholarships", text: "/RussiaScholarships"),
+    FF(image: "https://cdn.pixabay.com/photo/2016/01/16/14/56/buffer-1143485_640.jpg", Text: "Documents Required", text: "/RussiaDocuments"),
+    FF(image: "https://cdn.pixabay.com/photo/2018/09/04/10/06/man-3653346_640.jpg", Text: "Entrance Exams", text: "/RussiaExams"),];
+
   @override
   Widget build(BuildContext context) {
+    double hi = MediaQuery.of(context).size.height;
+    double wi = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.black,),
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade900,
+        title: Text("Russia",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 30,
+              color: Colors.white
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView.builder(itemCount: l.length,
+          itemBuilder: (context, i){
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15,
+                  vertical: 15),
+              child: Container(
+                height: hi/4,
+                width: wi,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.amber,
+                    image: DecorationImage(
+                        image: NetworkImage(l[i].image),
+                        fit: BoxFit.fill
+                    )
+                ),
+                child: Center(
+                  child: ListTile(
+                      title: TextButton(
+                        onPressed: (){
+                          Navigator.pushNamed(context, l[i].text);
+                        },
+                        child: Text(l[i].Text,
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white
+                          ),
+                        ),
+                      )
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
     );
+
   }
+}
+class FF{
+  final String image;
+  final String Text;
+  final String text;
+  FF({
+    required this.image,
+    required this.Text,
+    required this.text
+  }
+      );
 }
 
 class RussiaColleges extends StatefulWidget {
@@ -25,11 +92,14 @@ class RussiaColleges extends StatefulWidget {
 class _RussiaCollegesState extends State<RussiaColleges> {
   @override
   Widget build(BuildContext context) {
+    double hi = MediaQuery.of(context).size.height;
+    double wi = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.yellow,),
-    );
+
+  );
   }
 }
+
 
 class RussiaScholarships extends StatefulWidget {
   const RussiaScholarships({super.key});
@@ -41,6 +111,8 @@ class RussiaScholarships extends StatefulWidget {
 class _RussiaScholarshipsState extends State<RussiaScholarships> {
   @override
   Widget build(BuildContext context) {
+    double hi = MediaQuery.of(context).size.height;
+    double wi = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.tealAccent,
@@ -59,6 +131,8 @@ class RussiaDocuments extends StatefulWidget {
 class _RussiaDocumentsState extends State<RussiaDocuments> {
   @override
   Widget build(BuildContext context) {
+    double hi = MediaQuery.of(context).size.height;
+    double wi = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.cyan,
@@ -77,6 +151,8 @@ class RussiaExams extends StatefulWidget {
 class _RussiaExamsState extends State<RussiaExams> {
   @override
   Widget build(BuildContext context) {
+    double hi = MediaQuery.of(context).size.height;
+    double wi = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.red,
@@ -84,5 +160,3 @@ class _RussiaExamsState extends State<RussiaExams> {
     );
   }
 }
-
-
